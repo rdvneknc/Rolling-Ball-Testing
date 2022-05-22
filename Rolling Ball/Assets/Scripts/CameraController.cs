87d21eb51077a06b1cf73977b090b0ca158ad9e7
Rıdvan Ekinci <ridvanekinci92@gmail.com>
2022-05-22 03:16:21 +0300

    Update CameraController.cs

--- Rolling Ball/Assets/Scripts/CameraController.cs
+++ Rolling Ball/Assets/Scripts/CameraController.cs
@@ -9,19 +9,15 @@ public class CameraController : MonoBehaviour
 
     private Vector3 offset;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         offset = transform.position - player.transform.position;
-
-
     }
 
-    // Update is called once per frame
+
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-
-
     }
 }
